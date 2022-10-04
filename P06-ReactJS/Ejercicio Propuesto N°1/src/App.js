cb8a93c2a47c948a9c198bfb906bf9725f900ba6
RodrigoAlejandroDelgadilloPacheco <101844837+RodrigoAlejandroDelgadilloPacheco@users.noreply.github.com>
2022-10-04 03:57:57 -0500

    Cambiado
    cambiado los comentarios de index.html

--- P06-ReactJS/Ejercicio Propuesto N°1/src/App.js
+++ P06-ReactJS/Ejercicio Propuesto N°1/src/App.js
@@ -1,25 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  render() {
+      return <h2>Probando</h2>;
+  }
+};
+    const constante = <h3>Elementos</h3>
+      class Lista extends React.Component {
+        render() {
+                return <ul>
+                    <li>1</li>
+                    <li>2</li>
+                    <li>3</li>
+                    <li>4</li>
+                    <li>5</li>
+                </ul>;
+            }
+        }
+        
+        ReactDOM.render(<App />, document.getElementById('root'));
+        ReactDOM.render(constante, document.getElementById('root2'));
+        ReactDOM.render(<Lista />, document.getElementById('lista'));
+
+
 
 export default App;
