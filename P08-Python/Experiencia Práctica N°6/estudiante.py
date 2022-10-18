5630147e8ce450d5d6fcc17799b3f36f1fd66a6d
GianfrancoLozada <gianfranco.lozada@ucsm.edu.pe>
2022-10-18 03:29:27 -0500

    Experiencias 6, 7 y 8 (Por completar)

--- P08-Python/Experiencia Práctica N°6/estudiante.py
+++ P08-Python/Experiencia Práctica N°6/estudiante.py
@@ -12,23 +12,8 @@ class Estudiante(object):
         self.curso2 = _curso2
         self.curso3 = _curso3
         self.pension = _pension
-        self.historial = []
 
-    def entregarDatos(self):
-        print(" No. codigo: {} \n Nombres: {} \n Apellidos: {} \n Cursos Matriculados: \n     Curso 1: {} \n     Curso 2: {} \n     Curso 3: {} \n     Pensión: {}".format(self.codigo, self.nombres, self.apellidos, self.curso1, self.curso2, self.curso3, self.pension))
-    
-    def editarCursos(self, _curso1, _curso2, _curso3):
-        self.curso1 = _curso1
-        self.curso2 = _curso2
-        self.curso3 = _curso3
-
-    def editarPension(self, _pension):
-        self.pension = _pension
-
-        print("¡Registro Exitoso!")
-    def incluirEvento(self, _curso1, _curso2, _curso3):
-        return ("Rectificación de Matricula: Curso_1: {} Curso_2: {} Curso_3: {}".format(_curso1, _curso2, _curso3))
-    
+    #Método ingresarDatos()
     def ingresarDatos():
         print("Registro de Estudiante\n")
         codigo = int(input("Ingrese el número de código: "))
@@ -42,54 +27,40 @@ class Estudiante(object):
         objAlumno = Estudiante(codigo, nombres, apellidos, edad, curso1, curso2, curso3, pension)
         listaEstudiante.append(objAlumno)
 
+    #Método imprimirDatos()
     def imprimirDatos():
         print("Listado de Estudiante\n")
         for objAlumno in listaEstudiante:
             objAlumno.entregarDatos()
             input("Presione cualquier tecla para salir")
 
-
+    #Método matricular()
     def matricular():
         Estudiante.ingresarDatos()
         print("¡Matrícula Exitosa!")
         input("Presione cualquier tecla para salir")
 
+    #Método pagarPension()
     def pagarPension():
         print("Actualizar estado de pensión\n")
         codigo = int(input("Ingrese el número de código del estudiante a buscar: "))
         for objAlumno in listaEstudiante:
             if codigo == objAlumno.codigo:
-                pension = bool(input("Escribra 'True', si es que pagó y 'False' si es que no pagó: "))
-                objAlumno.editarPension(pension)
+                _pension = bool(input("Escribra 'True', si es que pagó y 'False' si es que no pagó: "))
+                if _pension == True:
+                    objAlumno.editarPension(_pension)
                 objAlumno.entregarDatos()
                 input("Presione cualquier tecla para salir")
             elif print("¡Número incorrecto!"):
                 pass
 
-    def buscarEstudiante():
-        print("Buscar Estudiante\n")
-        codigo = int(input("Ingrese el número de código del estudiante a buscar: "))
-        for objAlumno in listaEstudiante:
-            if codigo == objAlumno.codigo:
-                objAlumno.entregarDatos()
-                input("Presione cualquier tecla para salir")
-            elif print("¡Número incorrecto!"):
-                pass
+    def editarPension(self, _pension):
+        self.pension = _pension
+        print("¡Registro Exitoso!") 
 
-    def modificarCursos():
-        print("Modificar Cursos\n")
-        codigo = int(input("Ingrese el número de código del estudiante a buscar: "))
-        for objAlumno in listaEstudiante:
-            if codigo == objAlumno.codigo:
-                curso1 = input("Ingrese el Nuevo Curso 1: ")
-                curso2 = input("Ingrese el Nuevo Curso 2: ")
-                curso3 = input("Ingrese el Nuevo Curso 3: ")
-                objAlumno.editarCursos(curso1, curso2, curso3)
-                objAlumno.entregarDatos()
-                input("Presione cualquier tecla para salir")
-            elif print("¡Número incorrecto!"):
-                pass
- 
+    def entregarDatos(self):
+        print(" N° Código: {} \n Nombres: {} \n Apellidos: {} \n Cursos Matriculados: \n     Curso 1: {} \n     Curso 2: {} \n     Curso 3: {} \n     Pensión: {}".format(self.codigo, self.nombres, self.apellidos, self.curso1, self.curso2, self.curso3, self.pension))
+
 
 def salir():
     print("Salida inminente...!")
@@ -105,11 +76,9 @@ def main():
         print("")
         print("Seleccione una de las siguientes opciones:")
         print("1.- Matricular un Nuevo Estudiante")
-        print("2.- Mostrar Estudiante")
-        print("3.- Buscar Estudiante")
-        print("4.- Modificar Pensión")
-        print("5.- Modificar Cursos")
-        print("6.- Salir\n")
+        print("2.- Mostrar Estudiantes")
+        print("3.- Modificar Pensión")
+        print("4.- Salir\n")
 
         opcion = int(input("Opcion: "))
 
@@ -118,14 +87,12 @@ def main():
         elif opcion == 2:
             Estudiante.imprimirDatos()
         elif opcion == 3:
-            Estudiante.buscarEstudiante()
-        elif opcion == 4:
             Estudiante.pagarPension()
-        elif opcion == 5:
-            Estudiante.modificarCursos()
-        elif opcion == 6:
+        elif opcion == 4:
             salir()
+        
 
 if __name__ == '__main__':
     main()
 
+    
