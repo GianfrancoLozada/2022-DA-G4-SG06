5c7778e4e826dd217dfa825cc4a17ad008d0ce6c
NicolePintoTicona <101682143+NicolePintoTicona@users.noreply.github.com>
2022-10-18 06:46:02 -0500

    Experiencia 1 completo

--- P08-Python/Experiencia Práctica N°1/Exp1.py
+++ P08-Python/Experiencia Práctica N°1/Exp1.py
@@ -26,3 +26,17 @@ num = 1
 while(num!=0):
     num = int(input("Ingrese un numero: "))
     print(es_primo(num))
+
+#1c
+def calculo(a=3,b=5):
+        resul=a*b
+        print(resul)
+
+lista = (10,7)
+calculo(*lista)
+     
+def mifuncion(a,b=2,c=3):
+        print(a+b+c)
+        
+mifuncion(6,4)
+mifuncion(5)
