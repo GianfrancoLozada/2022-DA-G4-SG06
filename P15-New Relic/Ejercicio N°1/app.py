2f50e2a0ee7b9ddc290089db77fa178387ed8f40
RodrigoAlejandroDelgadilloPacheco <101844837+RodrigoAlejandroDelgadilloPacheco@users.noreply.github.com>
2022-12-07 04:58:41 +0000

    ejercicio 1
    este ejercicio he podido mas o menos cargar el flask, pero lo importante del New Relic a Python no pude entender la ubicacion del Script y en donde escribirlo, intente de varias combinaciones pero ninguna surgio efecto

---
+++ P15-New Relic/Ejercicio N°1/app.py
@@ -0,0 +1,43 @@
+from flask import Flask, jsonify, request,  
+import game_controller 
+from db import create_tables 
+ 
+app = Flask(__name__) 
+ 
+@app.route('/games', methods=["GET"]) 
+def get_games(): 
+games = game_controller.get_games() 
+return jsonify(games) 
+ 
+@app.route("/game", methods=["POST"]) 
+def insert_game(): 
+game_details = request.get_json() 
+name = game_details["name"] 
+price = game_details["price"] 
+rate = game_details["rate"] 
+result = game_controller.insert_game(name, price, rate) 
+return jsonify(result) 
+ 
+@app.route("/game", methods=["PUT"]) 
+def update_game(): 
+game_details = request.get_json() 
+id = game_details["id"] 
+name = game_details["name"] 
+price = game_details["price"] 
+rate = game_details["rate"] 
+result = game_controller.update_game(id, name, price, rate) 
+return jsonify(result) 
+ 
+@app.route("/game/<id>", methods=["DELETE"]) 
+def delete_game(id): 
+result = game_controller.delete_game(id) 
+return jsonify(result) 
+ 
+@app.route("/game/<id>", methods=["GET"]) 
+def get_game_by_id(id): 
+game = game_controller.get_by_id(id) 
+return jsonify(game) 
+ 
+if __name__ == "__main__": 
+create_tables() 
+app.run(host='0.0.0.0', port=8000, debug=False) 
---
+++ P15-New Relic/Ejercicio N°1/init.py
@@ -0,0 +1,3 @@
+if __name__ == "__main__":,  
+create_tables() 
+app.run(host='0.0.0.0', port=8000, debug=False) 
